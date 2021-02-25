92782715d78e81b8be3788ea7269b1543debd604
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2021-02-25 16:04:47 +0530

    Handle x version ranges in oval parser.
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- vulnerabilities/tests/test_suse.py
+++ vulnerabilities/tests/test_suse.py
@@ -96,7 +96,7 @@ class TestSUSEOvalParser(unittest.TestCase):
         assert pkg_set2 == {'cacti'}
 
 
-    def test_get_versionsrngs_from_state(self):
+    def test_get_version_ranges_from_state(self):
 
         assert len(self.parsed_oval.oval_document.getStates()) == 4
 
@@ -107,8 +107,8 @@ class TestSUSEOvalParser(unittest.TestCase):
         exp_range_2 = RangeSpecifier("<0:1.2.11-lp151.3.6")
         #In a full run we wont get exp_range1 because we won't obtain 
         #it's state due to filters to  avoid such tests in  the first place
-        assert self.parsed_oval.get_versionsrngs_from_state(state_1) == exp_range_1
-        assert self.parsed_oval.get_versionsrngs_from_state(state_2) == exp_range_2
+        assert self.parsed_oval.get_version_ranges_from_state(state_1) == exp_range_1
+        assert self.parsed_oval.get_version_ranges_from_state(state_2) == exp_range_2
     
     def test_get_urls_from_definition(self):
 
